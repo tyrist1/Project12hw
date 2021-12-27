b7dec00351180150186432d9f2c488b046ea01a9
Andrei <tyrist84@mail.ru>
2021-12-28 01:08:56 +0300

    исправил, 5 поиск скилов работает с ошибкой, надо найти 48-16

--- hw12/hw12.py
+++ hw12/hw12.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -15,6 +15,38 @@ def prof(id):
     for candidate in candidates:
         if candidate['id'] == int(id):
             return render_template("candidate.html", **candidate)
+@app.route('/list')
+def list_candidate():
+    with open('candidates.json') as f:
+        candidates = json.load(f)
+    return render_template("list.html", users=candidates)
 
+@app.route('/search/')
+def search_candidate():
+    name = request.args.get("name")
+    with open('candidates.json') as f:
+        candidates = json.load(f)
+    users = []
+    if name:
+        for candidate in candidates:
+            if name in candidate['name']:
+                users.append(candidate['name'])
+    return render_template("search_name.html", users=users, cnt=len(users))
+
+@app.route('/skill/<skill>')
+def search_skill(skill):
+    with open('candidates.json') as f:
+        candidates = json.load(f)
+    with open('settings.json') as f:
+        settings = json.load(f)
+    users = []
+    cnt = 0
+    for candidate in candidates:
+        if skill in candidate['skills']:
+            users.append(candidate['name'])
+            cnt += 1
+            if settings['limit'] == cnt:
+                return render_template("search_name.html", users=users, cnt=len(users))
+        return render_template("search_name.html", users=users, cnt=len(users))
 if __name__=="__main__":
     app.run()
